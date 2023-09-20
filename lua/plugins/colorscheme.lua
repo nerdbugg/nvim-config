94cc93272f2ad46ce761876cd3f1507889508198
chuyukang <17082643+Chuyukang@users.noreply.github.com>
2023-09-20 18:22:58 +0800

    ui: add colorscheme setting

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
