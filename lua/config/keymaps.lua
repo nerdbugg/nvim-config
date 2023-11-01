6b5986986af0c8fb5d5cfe96cc795e4896ae0ff8
chuyukang <17082643+Chuyukang@users.noreply.github.com>
2023-11-01 16:38:37 +0800

    plugin: add outline plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>h", "<cmd>%!xxd<cr>", { desc = "Show binary file in hex mode" })
+vim.keymap.set("n", "<leader>co", "<cmd>SymbolsOutline<cr>", { desc = "Show outline window" })
---
+++ lua/plugins/outline.lua
@@ -0,0 +1,10 @@
+return {
+  "simrat39/symbols-outline.nvim",
+  config = true,
+  cmd = "SymbolsOutline",
+  keys = {
+    "<leader>co"
+  },
+  opts = {
+  },
+}
