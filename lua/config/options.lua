4e2213ca82db065a56c0a1bf94607f5dc10eb396
nerdbugg <17082643+nerdbugg@users.noreply.github.com>
2026-02-19 18:53:58 +0800

    add tab=4 config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,7 @@
 
 vim.g.autoformat = false
 
+-- vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+-- vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+-- vim.o.softtabstop = 4 -- Number of spaces inserted insted of a TAB character
+-- vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
